1f1e7f923cd71427727299135042679153150c47
forwave-lab <59350683+forwave-lab@users.noreply.github.com>
2023-03-15 21:13:50 +0900

    no message

---
+++ Serial/Serial_Communication/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Serial_Communication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox_port = new System.Windows.Forms.ComboBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.button_disconnect = new System.Windows.Forms.Button();
+            this.textBox_send = new System.Windows.Forms.TextBox();
+            this.richTextBox_received = new System.Windows.Forms.RichTextBox();
+            this.label_send = new System.Windows.Forms.Label();
+            this.label_receive = new System.Windows.Forms.Label();
+            this.label_port = new System.Windows.Forms.Label();
+            this.button_send = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label_status = new System.Windows.Forms.Label();
+            this.textBox_send_hex = new System.Windows.Forms.RichTextBox();
+            this.richTextBox_received_hex = new System.Windows.Forms.RichTextBox();
+            this.label_comm = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox_port
+            // 
+            this.comboBox_port.FormattingEnabled = true;
+            this.comboBox_port.Location = new System.Drawing.Point(12, 56);
+            this.comboBox_port.Name = "comboBox_port";
+            this.comboBox_port.Size = new System.Drawing.Size(205, 20);
+            this.comboBox_port.TabIndex = 0;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(13, 170);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(97, 44);
+            this.button_connect.TabIndex = 1;
+            this.button_connect.Text = "연결하기";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.Button_connect_Click);
+            // 
+            // button_disconnect
+            // 
+            this.button_disconnect.Location = new System.Drawing.Point(122, 170);
+            this.button_disconnect.Name = "button_disconnect";
+            this.button_disconnect.Size = new System.Drawing.Size(97, 44);
+            this.button_disconnect.TabIndex = 1;
+            this.button_disconnect.Text = "연결끊기";
+            this.button_disconnect.UseVisualStyleBackColor = true;
+            this.button_disconnect.Click += new System.EventHandler(this.Button_disconnect_Click);
+            // 
+            // textBox_send
+            // 
+            this.textBox_send.Location = new System.Drawing.Point(301, 305);
+            this.textBox_send.Name = "textBox_send";
+            this.textBox_send.Size = new System.Drawing.Size(243, 21);
+            this.textBox_send.TabIndex = 2;
+            this.textBox_send.Text = "SBBC230001-10";
+            // 
+            // richTextBox_received
+            // 
+            this.richTextBox_received.Location = new System.Drawing.Point(301, 56);
+            this.richTextBox_received.Name = "richTextBox_received";
+            this.richTextBox_received.Size = new System.Drawing.Size(358, 61);
+            this.richTextBox_received.TabIndex = 3;
+            this.richTextBox_received.Text = "";
+            // 
+            // label_send
+            // 
+            this.label_send.AutoSize = true;
+            this.label_send.Location = new System.Drawing.Point(301, 287);
+            this.label_send.Name = "label_send";
+            this.label_send.Size = new System.Drawing.Size(29, 12);
+            this.label_send.TabIndex = 4;
+            this.label_send.Text = "송신";
+            // 
+            // label_receive
+            // 
+            this.label_receive.AutoSize = true;
+            this.label_receive.Location = new System.Drawing.Point(301, 38);
+            this.label_receive.Name = "label_receive";
+            this.label_receive.Size = new System.Drawing.Size(29, 12);
+            this.label_receive.TabIndex = 4;
+            this.label_receive.Text = "수신";
+            // 
+            // label_port
+            // 
+            this.label_port.AutoSize = true;
+            this.label_port.Location = new System.Drawing.Point(12, 38);
+            this.label_port.Name = "label_port";
+            this.label_port.Size = new System.Drawing.Size(90, 12);
+            this.label_port.TabIndex = 5;
+            this.label_port.Text = "COM 포트 설정";
+            // 
+            // button_send
+            // 
+            this.button_send.Location = new System.Drawing.Point(568, 303);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(91, 23);
+            this.button_send.TabIndex = 6;
+            this.button_send.Text = "보내기";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.Button_send_Click);
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Location = new System.Drawing.Point(15, 232);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(53, 12);
+            this.label_status.TabIndex = 7;
+            this.label_status.Text = "연결상태";
+            // 
+            // textBox_send_hex
+            // 
+            this.textBox_send_hex.Location = new System.Drawing.Point(301, 332);
+            this.textBox_send_hex.Name = "textBox_send_hex";
+            this.textBox_send_hex.Size = new System.Drawing.Size(358, 201);
+            this.textBox_send_hex.TabIndex = 9;
+            this.textBox_send_hex.Text = "";
+            // 
+            // richTextBox_received_hex
+            // 
+            this.richTextBox_received_hex.Location = new System.Drawing.Point(301, 123);
+            this.richTextBox_received_hex.Name = "richTextBox_received_hex";
+            this.richTextBox_received_hex.Size = new System.Drawing.Size(358, 161);
+            this.richTextBox_received_hex.TabIndex = 10;
+            this.richTextBox_received_hex.Text = "";
+            // 
+            // label_comm
+            // 
+            this.label_comm.AutoSize = true;
+            this.label_comm.Location = new System.Drawing.Point(10, 332);
+            this.label_comm.Name = "label_comm";
+            this.label_comm.Size = new System.Drawing.Size(65, 12);
+            this.label_comm.TabIndex = 11;
+            this.label_comm.Text = "송수신상태";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 552);
+            this.Controls.Add(this.label_comm);
+            this.Controls.Add(this.richTextBox_received_hex);
+            this.Controls.Add(this.textBox_send_hex);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.label_port);
+            this.Controls.Add(this.label_receive);
+            this.Controls.Add(this.label_send);
+            this.Controls.Add(this.richTextBox_received);
+            this.Controls.Add(this.textBox_send);
+            this.Controls.Add(this.button_disconnect);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.comboBox_port);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_port;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.Button button_disconnect;
+        private System.Windows.Forms.TextBox textBox_send;
+        private System.Windows.Forms.RichTextBox richTextBox_received;
+        private System.Windows.Forms.Label label_send;
+        private System.Windows.Forms.Label label_receive;
+        private System.Windows.Forms.Label label_port;
+        private System.Windows.Forms.Button button_send;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.RichTextBox textBox_send_hex;
+        private System.Windows.Forms.RichTextBox richTextBox_received_hex;
+        private System.Windows.Forms.Label label_comm;
+    }
+}
+
---
+++ Serial/Serial_Communication/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;  //시리얼통신을 위해 추가해줘야 함
+
+namespace Serial_Communication
+{
+    public partial class Form1 : Form
+    {
+        const byte STX = (byte)0x02;
+        const byte ETX = (byte)0x03;
+        const byte SOH = (byte)0x01;
+        const byte EOT = (byte)0x04;
+        const byte BEL = (byte)0x07;
+        const byte ENQ = (byte)0x05;
+        const byte SO = (byte)0x0E;
+        const byte SI = (byte)0x0F;
+        const byte ACK = (byte)0x06;
+        const byte NAK = (byte)0x15;
+        const byte ESC = (byte)0x1B;
+
+        const byte AT = (byte)0x40; //@
+        const byte SEMICOLON = (byte)0x3B; //;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)  //폼이 로드되면
+        {
+            comboBox_port.DataSource = SerialPort.GetPortNames(); //연결 가능한 시리얼포트 이름을 콤보박스에 가져오기 
+        }
+               
+        private void Button_connect_Click(object sender, EventArgs e)  //통신 연결하기 버튼
+        {
+            if (!serialPort1.IsOpen)  //시리얼포트가 열려 있지 않으면
+            {
+                serialPort1.PortName = comboBox_port.Text;  //콤보박스의 선택된 COM포트명을 시리얼포트명으로 지정
+                serialPort1.BaudRate = 9600;  //보레이트 변경이 필요하면 숫자 변경하기
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Parity = Parity.None;
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived); //이것이 꼭 필요하다
+                
+                serialPort1.Open();  //시리얼포트 열기
+
+                label_status.Text = "포트가 열렸습니다.";
+                comboBox_port.Enabled = false;  //COM포트설정 콤보박스 비활성화
+            }
+            else  //시리얼포트가 열려 있으면
+            {
+                label_status.Text = "포트가 이미 열려 있습니다.";
+            }
+        }
+                
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)  //수신 이벤트가 발생하면 이 부분이 실행된다.
+        {
+            System.Threading.Thread.Sleep(100);
+
+            this.Invoke(new EventHandler(MySerialReceived));  //메인 쓰레드와 수신 쓰레드의 충돌 방지를 위해 Invoke 사용. MySerialReceived로 이동하여 추가 작업 실행.
+        }
+
+     
+
+        private void Serial_Receive()
+        {
+
+            //수신 문자열 체크
+            if (richTextBox_received_hex.Text.IndexOf(textBox_send.Text) != -1)
+            {
+                label_comm.Text = "lot 정상 수신";
+            }
+            else if (richTextBox_received_hex.Text.IndexOf("E01") != -1)
+            {
+                label_comm.Text = "각인 취소";
+
+            }
+            else if (richTextBox_received_hex.Text.IndexOf("E03") != -1)
+            {
+                label_comm.Text = "각인 완료";
+
+            }
+            else if (richTextBox_received_hex.Text.IndexOf("B0000") != -1)
+            {
+                label_comm.Text = "각인 중";
+
+            }
+            else
+            {
+                label_comm.Text = "err code";
+            }
+        }
+
+        private void Button_send_Click(object sender, EventArgs e)  //보내기 버튼을 클릭하면
+        {
+            string strMarking = string.Empty;
+            string strMarking_rev = string.Empty;
+
+            strMarking = (char)ESC + "U" + textBox_send.Text + (char)SEMICOLON;     //센드데이터
+            strMarking_rev = (char)ESC + "?U" + (char)SEMICOLON;     //센드데이터 갔는지 확인 리시브 데이터
+
+            byte[] sdata = ASCIIEncoding.ASCII.GetBytes(strMarking);
+            byte[] sdata_rev = ASCIIEncoding.ASCII.GetBytes(strMarking_rev);
+
+            ListAdd(sdata);
+
+            this.SendSerialComm(sdata, sdata.Length);           //데이터 송신
+            //Delay(1000);                                         //만약 송신 중에 바로 또 송신이 될경우 주석 해제
+            this.SendSerialComm(sdata_rev, sdata_rev.Length);   //정상 수신 했는지 체크 송신 -> 보내면 바로 받은 데이터 수신보내줌
+
+        }
+
+        private void ListAdd(byte[] Msg)
+        {
+            string mMsg = string.Empty;
+
+            textBox_send_hex.Text = "";
+
+            foreach (byte b in Msg)
+            {
+                mMsg += string.Format("{0:X2}", b) + " ";
+            }
+            this.textBox_send_hex.Text = mMsg;
+        }
+
+
+        private void ListAdd_rev(int Msg)
+        {
+            
+            this.richTextBox_received_hex.Text = string.Format("{0:X2}", Msg); 
+        }
+
+
+        private void ListAdd_rev_1(string rev)
+        {
+
+            this.richTextBox_received_hex.Text = rev;
+        }
+
+        #region Receive
+        private void MySerialReceived(object s, EventArgs e)  //여기에서 수신 데이타를 사용자의 용도에 따라 처리한다.
+        {
+
+            string LotID = "";
+
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    //RcvBuffClear();
+                    LotID = string.Empty;
+
+                    //int nbyte = serialPort1.BytesToRead;
+                    //byte[] rbuff = new byte[nbyte];
+
+                    string sRcv = serialPort1.ReadExisting();
+
+                    //ListAdd_rev(nbyte);
+                    ListAdd_rev_1(sRcv);
+
+                    //if (nbyte > 0)
+                    //{
+                    //    serialPort1.Read(rbuff, 0, nbyte);
+                    //}
+
+                    //for (int i = 0; i < nbyte; i++)
+                    //{
+                    //    //arrSerialbuff.Add(rbuff[i]);
+                    //    if (i >= 0)
+                    //    {
+                    //        if (rbuff[i] == 1)
+                    //        {
+                    //            break;
+                    //        }
+                    //        else
+                    //        {
+                    //            LotID = LotID + string.Format("{0:X2}", (char)rbuff[i]);
+                    //        }
+                    //    }
+                    //}
+                    //richTextBox_received.Text = LotID;
+                    this.Serial_Receive();
+                }
+            }
+            catch (Exception ex)
+            {
+                //RcvBuffClear();
+                throw ex;
+            }
+
+
+        }
+        #endregion
+
+        #region send
+        public void SendSerialComm(byte[] SendComm_Packet, int len)
+        {
+            string Result = string.Empty;
+
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Write(SendComm_Packet, 0, len);
+                }
+                else
+                {
+                    serialPort1.Close();
+                    serialPort1.Open();
+                    serialPort1.Write(SendComm_Packet, 0, len);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return;
+                //throw ex;
+            }
+        }
+        #endregion
+
+        #region 연결해제
+        private void Button_disconnect_Click(object sender, EventArgs e)  //통신 연결끊기 버튼
+        {
+            if (serialPort1.IsOpen)  //시리얼포트가 열려 있으면
+            {
+                serialPort1.Close();  //시리얼포트 닫기
+
+                label_status.Text = "포트가 닫혔습니다.";
+                comboBox_port.Enabled = true;  //COM포트설정 콤보박스 활성화
+            }
+            else  //시리얼포트가 닫혀 있으면
+            {
+                label_status.Text = "포트가 이미 닫혀 있습니다.";
+            }
+        }
+        #endregion
+
+        #region Delay 함수
+        private static DateTime Delay(int MS)
+        {
+            DateTime ThisMoment = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+            DateTime AfterWards = ThisMoment.Add(duration);
+
+            while (AfterWards >= ThisMoment)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                ThisMoment = DateTime.Now;
+            }
+
+            return DateTime.Now;
+        }
+        #endregion
+    }
+}
